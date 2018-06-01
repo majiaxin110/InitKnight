8f2af7f9d2b3380d519a295b6346392065941feb
majiaxin110 <majiaxin110@gamil.com>
2018-06-01 23:53:51 +0800

    for back up ,not add too much

--- Classes/hero.cpp
+++ Classes/hero.cpp
@@ -2,6 +2,12 @@
 
 USING_NS_CC;
 
+Hero::Hero()
+{
+	isMoving = false;
+	heroDirection = 0;
+	cHeroSprite = nullptr;
+}
 void Hero::initHeroSprite()
 {
 	this->cHeroSprite = Sprite::create("playerHero.png");

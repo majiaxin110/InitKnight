c7254d51a16716173da33413781a2c506e99a68e
thedespairingone <34061560+thedespairingone@users.noreply.github.com>
2018-06-24 23:40:51 +0800

    Add files via upload

---
+++ BaseLayer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "cocos2d.h"
+#include <vector>
+#include "Bullet.h"
+#include "ipconfig.h"
+#include <string>
+USING_NS_CC;
+
+
+
+enum DataType
+{
+	POSITION,
+	ATTACK,
+};
+
+struct GameData
+{
+	int dataSize;
+	DataType dataType;
+	Vec2 position;
+	int heroface;
+	bool bulAttack;
+	float hero2Blood;
+	float heroBlood;
+};
+
+struct receiveTemp
+{
+	Vec2 receivePosition;
+	int heroface;
+	bool isNeedBullet;
+	float hero2Progress;
+	float heroProgress;
+};
